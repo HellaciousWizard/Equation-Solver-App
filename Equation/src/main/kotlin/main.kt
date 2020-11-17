f89b836ce25669acab3beffbcdc512cb7d8e6b38
TextTypeToast9 <bartick.chatterjee123@gmail.com>
2020-11-17 12:51:13 +0530

    Commit #1

--- Equation/src/main/kotlin/main.kt
+++ Equation/src/main/kotlin/main.kt
@@ -70,7 +70,7 @@ fun QuadraticEqn(l:List<String>)
     }
 }
 
-fun StrtoList(s:String): MutableList<String>
+fun StrtoList(s:String): MutableList<String> //I did a change here
 {
     var st = ""
     var i = 0
