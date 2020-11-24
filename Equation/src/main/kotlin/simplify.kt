71b443a9e6a1f084b727ee48a4c7da81609f03f2
TextTypeToast9 <72670913+TextTypeToast9@users.noreply.github.com>
2020-11-24 21:04:17 +0530

    Update simplify.kt

--- Equation/src/main/kotlin/simplify.kt
+++ Equation/src/main/kotlin/simplify.kt
@@ -8,33 +8,6 @@ class dset (ss: String)
     var ex = 0
 }
 
-fun stL(s:String): MutableList<String>
-{
-    var st = ""
-    var L = mutableListOf<String>()
-
-    var i = 0
-    while(i<s.length)
-    {
-        if (s[i].isDigit() || s[i] == '.')
-            st += s[i]
-        //else if ()
-        /*else {
-            if (st !== "") {
-                L.add(st)
-                st = ""
-            }
-            L.add(s[i].toString())
-        }
-        if (st != "" && (i == s.length - 1 || s[i + 1] == '-')) {
-            L.add(st)
-            st = ""
-        }*/
-            i++
-    }
-    return L
-}
-
 fun main() {
     var m = mainsolver()
     var l = mutableListOf("7x-y=4") //"-8x^2-67x+79x^2=-69"
